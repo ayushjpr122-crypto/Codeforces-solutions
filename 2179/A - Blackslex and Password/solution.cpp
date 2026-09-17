2091ed2c03ba707ccdc72121e6b7c072d4302c6d
AYUSH AGARWAL <ayush.jpr122@gmail.com>
2026-09-17 15:35:56 +0530

    Add Blackslex and Password [A] from Codeforces

---
+++ 2179/A - Blackslex and Password/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	    int t;
+	    cin>>t;
+	    while(t--){
+	        int k,x;
+	        cin>>k>>x;
+	        int n;
+	        n=k*x+1;
+	        cout<<n<<endl;
+	       
+	    }
+ 
+}
